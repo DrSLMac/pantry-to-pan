6bb85e64de1b11e874a60a350bd6295a1bcdc8cb
Maya <102932448+mayakappen@users.noreply.github.com>
2022-07-12 21:57:13 -0600

    Revert "Recipe repository class"

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,21 +1,8 @@
 class RecipeRepository {
-  constructor(cookbook) {
-    this.recipes = cookbook
+  constructor() {
+
     // One class to get you started!
   }
-  filterTag(tag) {
-    const recipes = this.recipes.filter((recipe) => {
-      
-      return recipe.tags.includes(tag)
-})
-    return recipes
-}
-filterName(name) {
-  const recipe = this.recipes.find((value) => {
-    return value.name.includes(name)
-  })
-  return recipe
-}
 }
 
 export default RecipeRepository;
